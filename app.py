eba505654a0be341858ff4b6572a08d00581e23e
miguellosoyo <44987101+miguellosoyo@users.noreply.github.com>
2022-06-07 22:47:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -572,7 +572,7 @@ if authentication_status:
             "subtext": f"Información Financiera de {licensee}",
             },
             "tooltip": {"trigger": "axis", "axisPointer": {"type": "shadow"}},
-            "legend": {"data": df.data['Concepto'].tolist(), "orient": "vertical", "x": "right"},
+            "legend": {"data": df.data['Concepto'].tolist(), "orient": "horizontal", "y": "bottom"},
             "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
             "xAxis": {"type": "value"},
             "yAxis": {
