9392eccc6e27782fefe35cfcce0698aca58b32f0
miguellosoyo <44987101+miguellosoyo@users.noreply.github.com>
2022-06-07 22:08:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -566,59 +566,25 @@ if authentication_status:
     st.caption(f'Información financiera de {licensee}.')
     
     options = {
-          "tooltip": {"trigger": "axis", "axisPointer": {"type": "shadow"}},
-          "legend": {
-              "data": ["Direct", "Mail Ad", "Affiliate Ad", "Video Ad", "Search Engine"]
-          },
-          "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
-          "xAxis": {"type": "value"},
-          "yAxis": {
-              "type": "category",
-              "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-          },
-          "series": [
-              {
-                  "name": "Direct",
-                  "type": "bar",
-                  # "stack": "total",
-                  "label": {"show": True},
-                  "emphasis": {"focus": "series"},
-                  "data": [320, 302, 301, 334, 390, 330, 320],
-              },
-              {
-                  "name": "Mail Ad",
-                  "type": "bar",
-                  # "stack": "total",
-                  "label": {"show": True},
-                  "emphasis": {"focus": "series"},
-                  "data": [120, 132, 101, 134, 90, 230, 210],
-              },
-              {
-                  "name": "Affiliate Ad",
-                  "type": "bar",
-                  # "stack": "total",
-                  "label": {"show": True},
-                  "emphasis": {"focus": "series"},
-                  "data": [220, 182, 191, 234, 290, 330, 310],
-              },
-              {
-                  "name": "Video Ad",
-                  "type": "bar",
-                  # "stack": "total",
-                  "label": {"show": True},
-                  "emphasis": {"focus": "series"},
-                  "data": [150, 212, 201, 154, 190, 330, 410],
-              },
-              {
-                  "name": "Search Engine",
-                  "type": "bar",
-                  # "stack": "total",
-                  "label": {"show": True},
-                  "emphasis": {"focus": "series"},
-                  "data": [820, 832, 901, 934, 1290, 1330, 1320],
-              },
-          ],
-      }
+        "tooltip": {"trigger": "axis", "axisPointer": {"type": "shadow"}},
+        "legend": {
+            "data": df.data['Concepto'].tolist()
+            },
+            "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
+            "xAxis": {"type": "value"},
+            "yAxis": {
+                "type": "category",
+                "data": df.data.columns.tolist()[1:],
+            },
+            "series": [{"name": f"{x}",
+                        "type": "bar",
+                        "stack": "total",
+                        "label": {"show": True},
+                        "emphasis": {"focus": "series"},
+                        "data": df.data.loc[df.data['Concepto']==x,:].values[0][1:].tolist(),
+                        } for x in df.data['Concepto'].tolist()
+                        ],
+            }
     st_echarts(options=options, height="500px")
 
 elif authentication_status==False:
