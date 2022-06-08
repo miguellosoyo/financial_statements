88f76bb879d080f9a4395de0626d01f7d8913838
miguellosoyo <44987101+miguellosoyo@users.noreply.github.com>
2022-06-08 03:40:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -654,11 +654,11 @@ if authentication_status:
                "series":[
                          {"name": f'{inv_type}',
                           "type": "bar",
-                          "data": investments[inv_type].round(2).values.tolist()
+                          "data": df_inv[inv_type].round(2).values.tolist()
                           },
                          {"name": f"{cf_type}",
                           "type": "line",
-                          "data": cash_flows[cf_type].round(2).values.tolist()
+                          "data": df_cf[cf_type].round(2).values.tolist()
                           }
                          ]
                }
