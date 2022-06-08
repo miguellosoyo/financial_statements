df6a2aec74a491d7404e7507b72c3197267aefc4
miguellosoyo <44987101+miguellosoyo@users.noreply.github.com>
2022-06-08 03:13:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -655,7 +655,7 @@ if authentication_status:
                          "axisLine": {
                          "show": True,
                          "lineStyle": {
-                             color: colors[2]
+                             "color": colors[1]
                              }
                              },
                          }
